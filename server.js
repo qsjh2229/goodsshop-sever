51dd779ceabdfa48806e2b524cd27ca79c04b60f
cheawook <qsjh2229@naver.com>
2023-08-07 11:56:11 +0900

    결제 기능 추가

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ const cors = require("cors");
 const app = express();
 const port = "8080";
 const models = require("./models");
+const multer = require('multer');
+app.use('/uploads',express.static("uploads"))
+const upload=multer({
+  storage:multer.diskStorage({
+    destination:function(req, file ,cb){
+cb(null,"uploads/")
+    },
+    filename:function(req, file ,cb){
+      cb(null,file.originalname)
+    }
+  })
+ 
+
+})
+
+
+
+
 app.use(express.json());
 app.use(cors()); // 브라우저의 cors 이슈를 막기 위해 사용하는 코드
 app.get("/products", (req, res) => {
@@ -72,7 +90,7 @@ app.get("/products", (req, res) => {
   models.Product.findAll({
    /*  limit:1 */
    order:[['createdAt','DESC']],
-   atrributes:["id","name","price","seller","imageUrl","createdAt","updatedAt" ]
+   atrributes:["id","name","price","seller","imageUrl","createdAt","updatedAt",'soldout' ]
   })
   .then((result)=>{
       console.log("PRODUCT:", result);
@@ -81,7 +99,7 @@ app.get("/products", (req, res) => {
       })
   }).catch((error)=>{
       console.error(error);
-      res.send('에러발생')
+      res.status(400).send('에러발생')
   })
 })
 
@@ -97,12 +115,34 @@ console.log("product:",result)
 res.send({product:result})
   }).catch((error)=>{
 console.error()
-res.send('상품조회 에러')
+res.status(400).send('상품조회 에러')
   })
 });
+app.post("/purchase/:id", (req, res)=>{
+  const { id } = req.params;
+  models.Product.update({
+    soldout:1
+  },{
+    where:{
+      id,
+    }
+  })
+  .then((result)=>{
+    res.send({
+      result:true,
+    })
+  })
+  .catch((error)=>{
+    console.error(error)
+    res.status(500).send('에러발생')
+  })
+})
 app.post("/products", (req, res) => {
   const body = req.body;
   const { name, price, seller, imageUrl, des } = body;
+  if( !name ||  !price ||  !seller ||  !imageUrl ||  !des){
+    res.send("모든필드를 입력해주세요")
+  }
   models.Product.create({
     name,
     price,
@@ -125,6 +165,16 @@ app.get("/products/:id", (req, res) => {
   const { id } = params;
   res.send(`id는 ${id} 입니다`);
 });
+
+
+app.post('/image', upload.single('image'),(req,res) =>{
+  const file =req.file;
+  res.send({
+    imageUrl:file.path
+  })
+})
+
+
 app.listen(port, () => {
   models.sequelize
     .sync()
